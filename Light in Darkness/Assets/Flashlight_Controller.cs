b40029ea29f01fc2be86957653e845508369c244
lokeshbawane0009 <lokeshbawane0009@gmail.com>
2021-02-22 00:31:03 +0530

    Working flashlight rotation

--- Light in Darkness/Assets/Flashlight_Controller.cs
+++ Light in Darkness/Assets/Flashlight_Controller.cs
@@ -13,6 +13,10 @@ public class Flashlight_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 direction = (mouseScreenPosition - (Vector2)transform.position).normalized;
+
+        transform.up = direction;
     }
 }

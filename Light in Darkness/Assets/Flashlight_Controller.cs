f3a27e28b96faf14979b6059622289a3ced6f8a8
lokeshbawane0009 <lokeshbawane0009@users.github.com>
2021-03-19 01:22:37 +0530

    Flashlight System updated with some tweaks

--- Light in Darkness/Assets/Flashlight_Controller.cs
+++ Light in Darkness/Assets/Flashlight_Controller.cs
@@ -1,21 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class Flashlight_Controller : MonoBehaviour
 {
-    Vector3 mouseScreenPosition;
+    public Vector3 mouseScreenPosition;
+    public Quaternion quaternion;
+    bool moving = false;
+    public Light2D flashlight;
+    public float speed = 5f;
+
+    float df_inner_angle, df_outer_angle, df_falloff_intensity, df_intensity;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        df_inner_angle = flashlight.pointLightInnerAngle;
+        df_outer_angle = flashlight.pointLightOuterAngle;
+        df_falloff_intensity = flashlight.falloffIntensity;
+        df_intensity = flashlight.intensity;
     }
+
 
     // Update is called once per frame
     void LateUpdate()
     {
+        FocusLight();
+
         mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = ((Vector2)mouseScreenPosition - (Vector2)transform.position).normalized;
-        transform.up = direction;
+        Vector3 direction = ((Vector2)mouseScreenPosition - (Vector2)transform.position).normalized;
+        //Debug.Log(direction);
+
+        if (moving)
+        {
+            transform.up = direction;
+            quaternion = transform.rotation;
+            Debug.Log(quaternion);
+        }
+        else
+        {
+            transform.up = direction;
+        }
+    }
+
+    private void FocusLight()
+    {
+        if (Input.GetButton("Fire2"))
+        {
+            flashlight.pointLightInnerAngle = Mathf.Lerp(flashlight.pointLightInnerAngle, 2f, Time.fixedDeltaTime * speed);
+            flashlight.pointLightOuterAngle = Mathf.Lerp(flashlight.pointLightOuterAngle, 5f, Time.fixedDeltaTime * speed);
+            //flashlight.falloffIntensity = Mathf.Lerp(flashlight.falloffIntensity, 0f, Time.fixedDeltaTime * speed);
+            flashlight.intensity = Mathf.Lerp(flashlight.intensity, 5, Time.fixedDeltaTime * speed);
+        }
+        else
+        {
+            flashlight.pointLightInnerAngle = Mathf.Lerp(flashlight.pointLightInnerAngle, df_inner_angle, Time.fixedDeltaTime * speed);
+            flashlight.pointLightOuterAngle = Mathf.Lerp(flashlight.pointLightOuterAngle, df_outer_angle, Time.fixedDeltaTime * speed);
+            //flashlight.falloffIntensity = Mathf.Lerp(flashlight.falloffIntensity, df_falloff_intensity, Time.fixedDeltaTime * speed);
+            flashlight.intensity = Mathf.Lerp(flashlight.intensity, df_intensity, Time.fixedDeltaTime * speed);
+        }
+    }
+
+    public void Setmoving(bool value)
+    {
+        moving = value;
     }
 }
--- Light in Darkness/Assets/Player_Controller.cs
+++ Light in Darkness/Assets/Player_Controller.cs
@@ -6,15 +6,16 @@ public class Player_Controller : MonoBehaviour
 {
     Rigidbody2D player_rigidbody;
     public Animator anim;
-
+    public Flashlight_Controller flashlight_Controller;
+    public ParticleSystem footsteepdust;
 
     [SerializeField]
-    float input_x, input_y;
+    float input_x, input_y,force;
 
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponentInChildren<Animator>();
+        anim = GetComponent<Animator>();
         player_rigidbody = GetComponent<Rigidbody2D>();
         input_x = 0;
         input_y = 0;
@@ -26,24 +27,40 @@ public class Player_Controller : MonoBehaviour
     void Update()
     {
         input_x = Input.GetAxisRaw("Horizontal");
+        
         Vector2 player_direction = new Vector2(input_x, input_y);
-        player_rigidbody.AddForce(player_direction * 2f,ForceMode2D.Force);
+        player_rigidbody.AddForce(player_direction * force, ForceMode2D.Force);
 
         scale = transform.localScale;
 
-        if (input_x > 0)
+        if (input_x > 0 || flashlight_Controller.mouseScreenPosition.x > transform.position.x)
+        {
             scale.x = 1;
-        else if (input_x < 0)
+        }
+        else if (input_x < 0 || flashlight_Controller.mouseScreenPosition.x < transform.position.x)
+        {
             scale.x = -1;
+        }
 
-        transform.localScale = scale;
+        transform.localScale = footsteepdust.transform.localScale =scale;
+
+        if (player_rigidbody.velocity.x==0)
+        {
+            footsteepdust.gameObject.SetActive(false);
+        }
+        else
+        {
+            footsteepdust.gameObject.SetActive(true);
+        }
 
         if (player_rigidbody.velocity.x != 0)
         {
+            flashlight_Controller.Setmoving(true);
             anim.SetBool("Walk", true);
         }
         else
         {
+            flashlight_Controller.Setmoving(false);
             anim.SetBool("Walk", false);
         }
 
@@ -51,5 +68,6 @@ public class Player_Controller : MonoBehaviour
         {
             player_rigidbody.AddForce(transform.up * 5f, ForceMode2D.Impulse);
         }
+
     }
 }
